1567cc0b367143f2b5628d990e3b939f8adc30a7
Renzo Fraga <kenkomarinho@gmail.com>
2026-03-09 23:55:08 -0300

    5

--- frontend/views.py
+++ frontend/views.py
@@ -29,7 +29,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('')
 
 def home_coordenador(request):
     if not request.user.is_authenticated:
